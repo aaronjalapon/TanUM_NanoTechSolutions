e0d40fe414c856851f78843755739c90477b3bf9
aaronjalapon <arquillanoaaron974@gmail.com>
2025-09-02 20:10:21 +0800

    kapoy na jud name ug commit

--- api/app.py
+++ api/app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For production, specify your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict")
 async def predict(request: Request):
